58a297c494c758f79d15f308806cc5052a217ebd
Mahdi1419 <99290119+Mahdi1419@users.noreply.github.com>
2022-02-22 01:52:18 +0330

    initial commit

--- scripts/login.js
+++ scripts/login.js
@@ -9,16 +9,16 @@ window.onload = async () => {
     const password = "m05041377"
 
     // Finding username input and send username and go next step
-    const username_input = await getData("#login_username", 500, 3000)
-    const username_send_btn = await getData("#login_password_continue", 500, 3000)
+    const username_input = await getData("#login_username")
+    const username_send_btn = await getData("#login_password_continue")
     let type_username = await type(username, 'username')
     await sleep(1000)
     username_send_btn.click()
 
 
     // Finding password input and login 
-    const password_input = await getData("#login_password", 500, 3000)
-    const login_btn = await getData("#login_control_continue", 500, 3000)
+    const password_input = await getData("#login_password"
+    const login_btn = await getData("#login_control_continue")
     let type_password = await type(password, 'password')
     await sleep(1000)
     login_btn.click()
